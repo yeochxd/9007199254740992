ef3eca96dd4ea066302f4950a26e013c5846bd5e
yeochxd <121787206+yeochxd@users.noreply.github.com>
2023-01-05 01:20:29 +0800

    Update extra_numbers.js

--- js/extra_numbers.js
+++ js/extra_numbers.js
@@ -1,3 +1,13 @@
+window.requestAnimationFrame(function () {
+  var game = new GameManager(8, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  game.actuator.addTile = function (tile) {
+    var self = this;
+    var wrapper   = document.createElement("div");
+    var inner     = document.createElement("div");
+    var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+    positionClass = this.positionClass(position);
+    var classes = ["tile", "tile-" + tile.value, positionClass];
+    this.applyClasses(wrapper, classes);
     inner.classList.add("tile-inner");
     switch (tile.value) {
     case 2:
@@ -37,3 +47,23 @@
       inner.textContent = "Mg";
       break;
     }
+    if (tile.previousPosition) {
+      window.requestAnimationFrame(function () {
+        classes[2] = self.positionClass({ x: tile.x, y: tile.y });
+        self.applyClasses(wrapper, classes);
+      });
+    } else if (tile.mergedFrom) {
+      classes.push("tile-merged");
+      this.applyClasses(wrapper, classes);
+      tile.mergedFrom.forEach(function (merged) {
+        self.addTile(merged);
+      });
+    } else {
+      classes.push("tile-new");
+      this.applyClasses(wrapper, classes);
+    }
+    wrapper.appendChild(inner);
+    this.tileContainer.appendChild(wrapper);
+    };
+    game.restart();
+});
